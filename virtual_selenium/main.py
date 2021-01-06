f485e9c8376b2066d40952cecea3a08dc46683bc
jeongbeen2 <jb2n95@kakao.com>
2021-01-06 15:36:36 +0900

    import save

--- virtual_selenium/main.py
+++ virtual_selenium/main.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
 
 KEYWORD = "buy domain"
 
